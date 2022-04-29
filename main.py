bd5749db0c0d2bcbbd9f54a45c87fb4ae7fb3e4a
jamilboashash <jamil.boashash@gmail.com>
2022-04-29 15:18:00 +1000

    Successfully processing SYNC requests and generating a reasonable response (still some bugs to fix).

--- main.py
+++ main.py
@@ -1,89 +1,44 @@
+from datetime import datetime
 from typing import Union
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, Response
+from post import Post
 from postRequest import PostRequest, process_sync, process_async
-from postResponse import PostResponse
 import subprocess
-import requests
-import json
 
 # define Flask web app
 app = Flask(__name__)
 
-
-def construct_command(post_request) -> list[str]:
-    """
-    Given the /text POST request, construct the chatterbox command to be executed
-    :param post_request: the incoming POST request to process
-    :return:
-    """
-
-    command = ['chatterbox', 'run']
-
-    # extract json data from request object
-
-    # --model
-    if 'model' in post_request:
-        model = post_request['model'].replace('.', '/')
-        command.extend(['--model', model])
-
-    # --input-file
-    # create new file and write 'message' contents into it, then give the filename to 'command'
-    filename = 'chatterbox-testing/test.in'
-    with open(filename, 'x') as file:
-        file.write(post_request['message'])
-    command.extend(['--input-file', filename])
-
-    # --out-path (leaving blank for default)
-    # --run-id todo - confirm details with tutors and implement!
-
-    return command
-
-
-def run_chatterbox(post_request):
+def run_chatterbox(post):
     """
     Given the incoming POST request, construct the chatterbox command to be executed and execute it.
-    :param post_request: the incoming POST request to process
+    :param post: the incoming POST request to process
     :return:
     """
-    command = construct_command(post_request)
-    print('command: ', command)
 
-    subprocess.run(command)
+    if post.operation == 'SYNC':
+        response = post.process_sync()
+        return response
 
-    print('subprocess finished...', flush=True)
+    elif post.operation == 'ASYNC':
+        response = post.process_async()
+        return response
 
-
-def post_request_is_valid(post_request) -> bool:
-
-    # make sure the request body is valid
-    print('Request arg:\n', post_request['message'])
-
-    if (1 <= len(post_request['message']) <= 280) and (post_request['operation'] == 'SYNC'):
-        print('This must be a SYNC operation')
-        return True
-    elif (len(post_request['message']) > 280) and (post_request['operation'] == 'ASYNC'):
-        print('This must be an ASYNC operation')
-        return True
-    else:
-        return False
-
-    # request is valid - assign values to postRequest class
-    # post_req = PostRequest(message, operation)
+    return 400
 
 
 def parse_post_request(post_request):
-
     print('POST request body:\n', post_request)
 
-    if post_request_is_valid(post_request):
+    # instantiate Post object
+    post = Post(post_request, datetime.now())
+
+    response = {}
+
+    if post.request_is_valid():
+        # todo post.add_to_queue()
         # execute chatterbox using POST request body
-        run_chatterbox(post_request)
-
-    # instantiate Text object and PostResponse object
-    # text = Text(message, operation, model)
-
-    # response = PostResponse(1, text, 'COMPLETED', './out/*.wav', os.path.getmtime('./out/*.wav'))
+        response = run_chatterbox(post)
 
     # if operation == 'SYNC':
     #     process_sync()
@@ -94,7 +49,7 @@ def parse_post_request(post_request):
     #     # todo - clarify with tutor how to handle this case - exit_with_code(Exit.BAD_OPERATION) ?
     #     pass
 
-    return "Generic Test Response! "  # response
+    return response
 
 
 def process_get_request(get_request):
@@ -110,12 +65,10 @@ def is_valid_json():
 
 
 @app.route('/text', methods=['GET', 'POST'])
-def process_text_request() -> Union[int, str]:
+def process_text_request() -> Union[int, dict]:
 
     if request.get_json() is None:
         return 400  # if the request is NOT valid json then return error code 400
-
-    response = None
 
     # if we get here we definitely received valid json
     if request.method == 'POST':
---
+++ post.py
@@ -0,0 +1,129 @@
+import glob
+import itertools
+import json
+import os
+import subprocess
+
+
+class Post:
+    id_iter = itertools.count()
+
+    def __init__(self, post_request, created_at):
+        self.request_id = next(self.id_iter)
+        self.message = post_request['message']
+        self.operation = post_request['operation']
+        if 'model' in post_request:
+            self.model = post_request['model']
+        else:
+            self.model = 'tts_models/en/ljspeech/glow-tts'
+        self.status = None
+        self.resource = None
+        self.created_at = created_at
+        self.processed_at = None
+        self.error = None
+
+    def generate_response(self, message, operation, model, status, resource, created_at, processed_at, error):
+        response = {"id": str(next(self.id_iter)),
+                    "message": message,
+                    "operation": operation,
+                    "model": model,
+                    "status": status,
+                    "resource": resource,
+                    "created_at": created_at,
+                    "processed_at": processed_at,
+                    "error": error}
+        return response
+
+    def construct_command(self) -> list[str]:
+        """
+        Construct the chatterbox command to be executed
+        :return:
+        """
+        command = ['chatterbox', 'run']
+
+        # --model
+        if hasattr(self, 'model'):
+            model = self.model.replace('.', '/')
+            command.extend(['--model', model])
+
+        # --input-file
+        # create new file and write 'message' contents into it, then give the filename to 'command'
+        filename = 'temp/message.in'  # todo update this
+        with open(filename, 'x') as file:
+            file.write(self.message)
+        command.extend(['--input-file', filename])
+
+        # --out-path (leaving blank for default)
+        # --run-id todo - confirm details with tutors and implement!
+
+        return command
+
+    def construct_response(self):
+
+        # read json file
+        files = glob.glob('./out/*.json')
+        latest = max(files, key=os.path.getctime)
+        print('Latest file:\n', latest)
+        with open(latest, 'r') as file:
+            json_file = json.load(file)
+
+        print('json file:\n', json_file)
+        # process_json_file(json_file)
+
+        self.status = 'COMPLETED'
+        if 'result' in json_file:
+            self.resource = json_file['result']['wav']
+            print('Resource:\n', json_file['result']['wav'])
+        self.processed_at = os.path.getctime(latest)
+
+        if 'error' in json_file:
+            self.error = json_file['result']['error']
+            print('Error:\n', self.error)
+
+        example = {"id": "2bf02bea-c0fd-43ee-b5f0-77e7117b2261",
+                   "message": "Hello, CSSE6400!",
+                   "operation": "SYNC",
+                   "model": "tts_models.en.ljspeech.glow-tts",
+                   "status": "COMPLETED",
+                   "resource": "http://storage:4566/chatterbox-texts/2bf02bea-c0fd-43ee-b5f0-77e7117b2261.wav",
+                   "created_at": "2022-04-26T07:22:10.467Z",
+                   "processed_at": "2022-04-26T07:22:19.171Z",
+                   "error": 'null'}
+
+        response = self.generate_response(self.message, self.operation, self.model, self.status,
+                                          self.resource, self.created_at, self.processed_at, self.error)
+
+        return str(response)
+
+    def request_is_valid(self) -> bool:
+        """
+        Make sure the request body is valid.
+        :return:
+        """
+
+        print('Length of message:\n\t', len(self.message))
+
+        if (0 < len(self.message) <= 280) and (self.operation == 'SYNC'):
+            print('We get here!', flush=True)
+            return True
+
+        elif (len(self.message) > 280) and (self.operation == 'ASYNC'):
+            return True
+
+        # todo - validate the model field
+
+        return False
+
+    def process_sync(self):
+
+        command = self.construct_command()
+        print('command: ', command)
+        subprocess.run(command)
+        print('subprocess finished...', flush=True)
+
+        response = self.construct_response()
+        return response
+
+    def process_async(self):
+        # todo - process asynchronously
+        pass

4fb650e35512f09fd53f809e4e1320bb6d55a5d7
Jamil <jamil.boashash@gamil.com>
2022-05-19 17:16:39 +1000

    removed the docker push line

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
 
-# 1. Your Git repository will be checked out locally.
-# 2. AWS credentials will be copied into your repository in the top-level directory, in a file called credentials.
-# 3. The script deploy.sh in the top-level of the repository will be run.
-# 4. The deploy.sh script must create a file named api.txt which contains the URL where your API is deployed to.
-# 5. We will run automated functionality and load-testing on the URL provided in the api.txt file.
-
-echo "${GH_PAT}" | docker login ghcr.io -u jamilboashash --password-stdin
-
 terraform apply -auto-approve
-
-
